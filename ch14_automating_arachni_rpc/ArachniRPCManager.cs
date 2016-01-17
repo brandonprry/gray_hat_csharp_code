f323ea72b27f373b3b3d5e8bc9bac441c5dcbc15
Brandon Perry <bperry.volatile@gmail.com>
2016-01-16 20:18:56 -0600

    Add the RPC as a part of 14, let's commit to it with a commit!

---
+++ ch14_automating_arachni_rpc/ArachniRPCManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using MsgPack;
+using System.Collections.Generic;
+
+namespace ch14_automating_arachni_rpc
+{
+	public class ArachniRPCManager : IDisposable
+	{
+		ArachniRPCSession _session;
+
+		public ArachniRPCManager (ArachniRPCSession session)
+		{
+			if (!session.IsInstanceStream)
+				throw new Exception ("Session must be using an instance stream, not a dispatcher stream");
+
+			_session = session;
+		}
+
+		public MessagePackObject StartScan (string url, string checks = "*")
+		{
+			Dictionary<string, object> args = new Dictionary<string, object> ();
+			args ["url"] = url;
+			args ["checks"] = checks;
+			args ["audit"] = new Dictionary<string, object> ();
+			((Dictionary<string, object>)args ["audit"]) ["elements"] = new object[] { "links", "forms" };
+
+			return _session.ExecuteCommand ("service.scan", new object[]{ args }, _session.Token);
+		}
+
+		public MessagePackObject GetProgress(List<uint> digests = null) {
+			Dictionary<string, object> args = new Dictionary<string, object> ();
+			args ["with"] = "issues";
+			if (digests != null) {
+				args ["without"] = new Dictionary<string, object> ();
+				((Dictionary<string, object>)args ["without"]) ["issues"] = digests.ToArray();
+			}
+			return _session.ExecuteCommand ("service.progress", new object[] { args }, _session.Token);
+		}
+
+		public MessagePackObject IsBusy ()
+		{
+			return _session.ExecuteCommand ("service.busy?", new object[]{ }, _session.Token);
+		}
+
+		public void Dispose ()
+		{
+			_session.Dispose ();
+		}
+	}
+}
+
