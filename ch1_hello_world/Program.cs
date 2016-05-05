1925d9c72e2a00ca3ab6a4969464e7601a85a87f
Brandon Perry <bperry.volatile@gmail.com>
2016-05-05 09:32:33 -0500

    hello world

---
+++ ch1_hello_world/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ch1_hello_world
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string hello = "Hello World!";
+			DateTime now = DateTime.Now;
+			Console.Write(hello);
+			Console.WriteLine (" The date is " + now.ToLongDateString());
+		}
+	}
+}
