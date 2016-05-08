5496cf47059378a179e819d05e856382042abbc7
Brandon Perry <bperry.volatile@gmail.com>
2016-05-08 10:49:10 -0500

    Add decompile example from decompilation chapter

---
+++ ch10_decompile_example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ch10_hello_world
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine(2 + 2);
+		}
+	}
+}
